2b5fbf93c2702622519ba3f3deb289d92e63dd97
ooooocean <hophuong.ocean@gmail.com>
2025-02-08 02:23:10 +0000

    linting

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-
+'''Runs the bot upon code execution'''
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,
                                    text="I'm a bot, please talk to me!")
