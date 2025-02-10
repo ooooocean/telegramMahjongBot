d2ffa36b91d28d7972ef5baae46c0ba9077a484e
ooooocean <hophuong.ocean@gmail.com>
2025-02-10 03:00:21 +0000

    added dicts for base mahjong tiles

---
+++ logic.py
@@ -0,0 +1,150 @@
+tiles_dict = {
+    "red_dragon": "🀄",
+    "green_dragon": "🀅",
+    "white_dragon": "🀆",
+    "character_one": "🀇",
+    "character_two": "🀈",
+    "character_three": "🀉",
+    "character_four": "🀊",
+    "character_five": "🀋",
+    "character_six": "🀌",
+    "character_seven": "🀍",
+    "character_eight": "🀎",
+    "character_nine": "🀏",
+    "bamboo_one": "🀐",
+    "bamboo_two": "🀑",
+    "bamboo_three": "🀒",
+    "bamboo_four": "🀓",
+    "bamboo_five": "🀔",
+    "bamboo_six": "🀕",
+    "bamboo_seven": "🀖",
+    "bamboo_eight": "🀗",
+    "bamboo_nine": "🀘",
+    "dots_one": "🀙",
+    "dots_two": "🀚",
+    "dots_three": "🀛",
+    "dots_four": "🀜",
+    "dots_five": "🀝",
+    "dots_six": "🀞",
+    "dots_seven": "🀟",
+    "dots_eight": "🀠",
+    "dots_nine": "🀡",
+    "flower_one": "🀢",
+    "flower_two": "🀣",
+    "flower_three": "🀤",
+    "flower_four": "🀥",
+    "season_one": "🀦",
+    "season_two": "🀧",
+    "season_three": "🀨",
+    "season_four": "🀩"
+}
+
+winds_dict = {
+    "east_wind": {
+        "tile_unicode": "🀀",
+        "name": "East Wind",
+        "type": "wind",
+        "num_val": "",
+        "direction": "east"
+    },
+    "south_wind": {
+        "tile_unicode": "🀁",
+        "name": "South Wind",
+        "type": "wind",
+        "num_val": "",
+        "direction": "south"
+    },
+    "west_wind": {
+        "tile_unicode": "🀂",
+        "name": "West Wind",
+        "type": "wind",
+        "num_val": "",
+        "direction": "west"
+    },
+    "north_wind": {
+        "tile_unicode": "🀃",
+        "name": "North Wind",
+        "type": "wind",
+        "num_val": "",
+        "direction": "north"
+    },
+}
+
+dragons_dict = {
+    "white_dragon": {
+        "tile_unicode": "🀄",
+        "name": "White Dragon",
+        "type": "dragon",
+        "num_val": "",
+    },
+    "green_dragon": {
+        "tile_unicode": "🀅",  # Using a placeholder, as the green dragon might not have a standard emoji
+        "name": "Green Dragon",
+        "type": "dragon",
+    },
+    "red_dragon": {
+        "tile_unicode": "🀆",  # Using a placeholder, as the red dragon might not have a standard emoji
+        "name": "Red Dragon",
+        "type": "dragon",
+    },
+}
+
+dots_dict = {
+    "one_dot": {
+        "tile_unicode": "🀌",
+        "name": "one dot",
+        "type": "dot",
+        "num_val": 1
+    },
+    "two_dots": {
+        "tile_unicode": "🀍",
+        "name": "two dots",
+        "type": "dot",
+        "num_val": 2
+    },
+    "three_dots": {
+        "tile_unicode": "🀎",
+        "name": "three dots",
+        "type": "dot",
+        "num_val": 3
+    },
+    "four_dots": {
+        "tile_unicode": "🀏",
+        "name": "four dots",
+        "type": "dot",
+        "num_val": 4
+    },
+    "five_dots": {
+        "tile_unicode": "🀐",
+        "name": "five dots",
+        "type": "dot",
+        "num_val": 5
+    },
+    "six_dots": {
+        "tile_unicode": "🀑",
+        "name": "six dots",
+        "type": "dot",
+        "num_val": 6
+    },
+    "seven_dots": {
+        "tile_unicode": "🀒",
+        "name": "seven dots",
+        "type": "dot",
+        "num_val": 7
+    },
+    "eight_dots": {
+        "tile_unicode": "🀓",
+        "name": "eight dots",
+        "type": "dot",
+        "num_val": 8
+    },
+    "nine_dots": {
+        "tile_unicode": "🀔",
+        "name": "nine dots",
+        "type": "dot",
+        "num_val": 9
+    },
+}
+
+def tiles_init():
+    deck = []
