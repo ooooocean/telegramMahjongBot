798179a2fb1a491a6a644cad9e9926a43cadb143
ooooocean <hophuong.ocean@gmail.com>
2025-02-11 09:51:35 +0000

    updated tests for deal_tiles

--- test_logic.py
+++ test_logic.py
@@ -4,3 +4,7 @@ import logic
 def test_dealt_tiles_total_correctly():
     hands, deck = logic.deal_tiles()
     assert len(deck) + len(hands[0]) + len(hands[1]) + len(hands[2]) + len(hands[3]) == 148
+    assert len(hands[0]) == 14
+    assert len(hands[1]) == 13
+    assert len(hands[2]) == 13
+    assert len(hands[3]) == 13
